3ebc173e2a288b3f853bcee87c6a0a4322a1a42c
palevominimale <badtri.orchestra@gmail.com>
2022-11-25 18:25:53 +0100

    added pager for favorites and current search now player is not updating when station is clicked, check it asap

--- app/src/main/java/app/seals/radio/ui/screens/main/MainScreen.kt
+++ app/src/main/java/app/seals/radio/ui/screens/main/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
